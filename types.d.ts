ee4401a4bed5318a8ddd63fec6aa37e77ca182cb
Yuresh Majhi <yureshmee@gmail.com>
2026-01-12 15:56:35 +0545

    img declarations

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,21 @@
 declare module "react-day-picker";
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
