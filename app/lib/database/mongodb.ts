fd82b6304c8597d78ee67d5fc73dbcd8fb2ba039
Yuresh Majhi <yureshmee@gmail.com>
2026-03-24 16:44:01 +0545

    check mongo url at runtime not deploy time

--- app/lib/database/mongodb.ts
+++ app/lib/database/mongodb.ts
@@ -10,19 +10,23 @@ declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-if (!uri) {
-  throw new Error("Please add your MongoDB URI to .env");
+function getClientPromise() {
+  if (!uri) {
+    throw new Error("Missing MONGO_URL environment variable");
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    if (!global._mongoClientPromise) {
+      client = new MongoClient(uri, options);
+      global._mongoClientPromise = client.connect();
+    }
+    return global._mongoClientPromise;
+  } else {
+    client = new MongoClient(uri, options);
+    return client.connect();
+  }
 }
 
-if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+clientPromise = getClientPromise();
 
 export default clientPromise;
